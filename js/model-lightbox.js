e705e8d98bb84763882eedea09e310a14f5edf27
Michael Peck <mrpeckjr@gmail.com>
2021-09-22 17:52:30 -0500

    Fixed lightboxes so they appear.

--- js/model-lightbox.js
+++ js/model-lightbox.js
@@ -3,12 +3,11 @@ var selectedDemo;
 var selectedCaption;
 var slideIndex = 1;
 
-function openModal(modalID, modalName, captionName) {
+function openModal(modalID, modalName, captionName, demoName) {
   selectedModal = modalName;
-  alert("Open modal name is: " + modalName);
-  selectedCaption = captionName;
+  selectedDemo = demoName;
+  selectedCaption = captionName;  
   document.getElementById(modalID).style.display = "block";
-  alert("Open modal's slides length is: " + document.getElementsByClassName(modalName).length);
   showSlides(slideIndex, selectedModal, selectedDemo);
 }
 function closeModal(modalID) {
@@ -21,16 +20,19 @@ function currentSlide(n) {
   showSlides(slideIndex = n, selectedModal);
 }
 function showSlides(n, modalName) {
-  alert("Modal name is: " + modalName);
   var i;
   var slides = document.getElementsByClassName(modalName);
-  //var captionText = document.getElementById(modalCaption);
-  alert('Slides length is: ' + slides.length);
+  var dots = document.getElementsByClassName(selectedDemo);
+  var captionText = document.getElementById(selectedCaption);
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
   slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
